ee2f54e0d6d7cfc7034de9b1cf60fc2fb099fc60
navid8181 <nr5391894@gmail.com>
2024-12-08 16:47:25 +0330

    add comment

--- cross_word.py
+++ cross_word.py
@@ -1,6 +1,6 @@
 import re
 
-
+#ایجاد آرایه دوبعدی
 def initializeBlocks(blocks=[],ROW=5,COL=5) :
     for i in range (ROW) :
         col = [];
@@ -10,12 +10,13 @@ def initializeBlocks(blocks=[],ROW=5,COL=5) :
 
         blocks.append(col)    
 
+#چاپ آرایه
 def printArray(arr=[]):
     for i in range(len(arr)) :
         for j in  range (len(arr[0])) :
             print (arr[i][j],end=' ')
         print('\n')
-
+#قرار دادن کلمات به روش افقی در ارایه
 def setHorizontalValues (i,j,x1,x2,blocks=[[]],values=[]) :
     
     length = x2-x1+1
@@ -26,7 +27,7 @@ def setHorizontalValues (i,j,x1,x2,blocks=[[]],values=[]) :
     for k in range (length) :
         blocks[i][k+j] = values[valuesIndex]    
         valuesIndex  += 1
-
+#گرفتن کلمات به صورت افقی
 def getHorizontalValues (i,j,x1,x2,blocks=[[]]) :
     
     values = []
@@ -39,6 +40,7 @@ def getHorizontalValues (i,j,x1,x2,blocks=[[]]) :
       
     return values     
 
+#قرار دادن کلمات به روش عمودی در ارایه
 def setVerticalValues (i,j,y1,y2,blocks=[[]],values=[]) :
     
     length = y2-y1+1
@@ -49,7 +51,7 @@ def setVerticalValues (i,j,y1,y2,blocks=[[]],values=[]) :
     for k in range (length) :
         blocks[k+i][j] = values[valuesIndex]    
         valuesIndex  += 1 
-
+#گرفتن کلمات به صورت عموذی
 def getVerticalValues (i,j,y1,y2,blocks=[[]]) :
     
     length = y2-y1+1
@@ -60,7 +62,9 @@ def getVerticalValues (i,j,y1,y2,blocks=[[]]) :
         values.append(blocks[k+i][j])
 
     return values
-    
+  
+  #گرفتن  ابتدا و انتهای بین دو (# #) در آرایع به صورت افقی  
+
 def getHorizontalRangeIndex(i,j,blocks=[[]]):
     rightIndex = j
     leftIndex = j
@@ -80,6 +84,8 @@ def getHorizontalRangeIndex(i,j,blocks=[[]]):
         leftIndex +=1
     
     return (leftIndex,rightIndex)
+
+#گرفتن  ابتدا و انتهای بین دو (# #) در آرایع به صورت عموددی  
 
 def getVerticalRangeIndex(i,j,blocks=[[]]):
         upperIndex = i
@@ -101,6 +107,9 @@ def getVerticalRangeIndex(i,j,blocks=[[]]):
         
         return (bottomIndex,upperIndex)
 
+
+#سرجچ در آرایه  input 
+#با یک ریجکس خاص
 def searchInArray (regex="",input=[]):
     output = []
     for i in range(len(input)):
@@ -110,6 +119,7 @@ def searchInArray (regex="",input=[]):
             output.append(input[i])
     return output
 
+#گرفتن ریجکس آن سطر برای هندل کردن کلمات مناسب آن سطر
 def getHorizontalRegexValid(i,j,blocks=[[]]) :
     # for j
     left,right = getHorizontalRangeIndex(i,j,blocks)
@@ -124,7 +134,7 @@ def getHorizontalRegexValid(i,j,blocks=[[]]) :
     horizontalValidRegexValue +="$"            
     return  horizontalValidRegexValue 
 
-
+#گرفتن ریجکس آن ستون برای هندل کردن کلمات مناسب آن ستون
 def getVerticalRegexValid(i,j,blocks=[[]]) :
     # for j
     bottom,upper = getVerticalRangeIndex(i,j,blocks)
@@ -139,24 +149,28 @@ def getVerticalRegexValid(i,j,blocks=[[]]) :
     verticalValidRegexValue +="$"                
     return  verticalValidRegexValue  
 
-
+#به درست آوردن کلمات مناسب یک سطر
+#روس به این شکل کلمه باید مناسب آن سطر انتخاب شده باشه
+#بع ازای هر حرف باید با ستون خود هم هماهنک باشع
 def getHorizontalWordCandidate(i,j,blocks=[[]],input=[]):
     
     leftIndex,rightIndex = getHorizontalRangeIndex(i,j,blocks)
     horizontalRegex = getHorizontalRegexValid(i,j,blocks)
     horizontalValidWords = searchInArray(horizontalRegex,input)
     
+    #گرفتن مفادیر مناسب هر سطح
     lastValues = getHorizontalValues(i,leftIndex,leftIndex,rightIndex,blocks)
     candidateWords = []
-    
+    #بع ازای هرکلمه
     for idx in range(len(horizontalValidWords)) :
         isCandidate = True
         values = [char for char in horizontalValidWords[idx]]
-    
+
+        #ست کردن آن
         setHorizontalValues(i,leftIndex,leftIndex,rightIndex,blocks,values)
 
         length = rightIndex - leftIndex + 1
-        
+        #چک کردن آیا با ستون هماهنک هست هر حرف یا نه
         for k in range(length) :
             upper,bottom = getVerticalRangeIndex(i,k+leftIndex,blocks)
             if upper == bottom :
@@ -165,7 +179,7 @@ def getHorizontalWordCandidate(i,j,blocks=[[]],input=[]):
             if (len(searchInArray(verticalRegex,input)) == 0) :
                 isCandidate = False
                 break
-        
+        #اضافع به لیست کلمات قابل انتخاب
         if isCandidate :
             candidateWords.append(horizontalValidWords[idx])
     
@@ -173,7 +187,7 @@ def getHorizontalWordCandidate(i,j,blocks=[[]],input=[]):
     return candidateWords   
 
 
-
+#مثل بالا فقط بزعکس
 def getVerticalWordCandidate(i,j,blocks=[[]],input=[]):
     
     bottom,upper = getVerticalRangeIndex(i,j,blocks)
@@ -206,7 +220,7 @@ def getVerticalWordCandidate(i,j,blocks=[[]],input=[]):
     
     setVerticalValues(bottom,j,bottom,upper,blocks,lastValues)
     return candidateWords         
- 
+#گرفتن ایندکس بعدی  پر نشده
 def getNextIndex (i,j,blocks=[[]]):
     for i in range(len(blocks)) :
         for j in  range (len(blocks[0])) :
@@ -216,7 +230,7 @@ def getNextIndex (i,j,blocks=[[]]):
     
     return (-1,-1)
    
-
+#گرفتن تعداد ستون ها پر نشده
 def getCount (i,j,blocks=[[]]):
     count = 0
     for i in range(len(blocks)) :
@@ -225,44 +239,51 @@ def getCount (i,j,blocks=[[]]):
             if (blocks[i][j] == '-'):
                 count +=1  
     return count    
-    
+   
+#حل جدول    
+#جدول یا به حالت افقی حل می شود با عمودی
 def crossWordSolver(i,j,blocks=[[]],input=[],count=20) :
     
-
+    #جدول به طرز کامل پر شده است
     if count == 0 :
-        print("win")
-   
+
         return True
-    
+    #به ازای هر کلمه ای که می توان در سطر افقی گزاشت
     for word in getHorizontalWordCandidate(i,j,blocks,input) :
         
          leftIndex,rightIndex = getHorizontalRangeIndex(i,j,blocks) 
          values = [char for char in word]
          
+         # به دست آوردن مقادیر جاگزاری شده
          changedValues = []
          for chgVal in range(rightIndex - leftIndex + 1 ):
              if blocks[i][leftIndex + chgVal] == '-' :
                 changedValues.append((i,leftIndex+chgVal))
          
-         setHorizontalValues(i,leftIndex,leftIndex,rightIndex,blocks,values)   
+         #اضافه کردن کلمه
+         
+         setHorizontalValues(i,leftIndex,leftIndex,rightIndex,blocks,values)  
+         #محاسبه ی اندازع واقعی کلمات تغییر کرده 
          realLength = len(changedValues)
-         
+         #گرفتن حفره خالی بعدی که مسیله حل نشده
          x,y = getNextIndex(i,j,blocks)  
-         
+         #به دست آوردن نعداد خانه های پر نشده
          count = count - realLength
+         #حل مسیله برای خانه بعدی بع صورت بازگشتی
          check = crossWordSolver(x,y,blocks,input,count) 
+         #نشان دادن جواب در صورت پیدا شدن
          if (check) :
              return True
          
 
-         
+         #اضافه کردن خانه به درخت در صورت شکسته خوردن در حل مسیله
          count += realLength
-        
+        #برگردادن جدول بع حالت قبل و امتحان حالت ها ی دیگر
          for (l,m) in changedValues :
              blocks[l][m] = '-'
     
 
-
+    #شبیه بالا برای حل جدول به صورت عمودی
     for word in getVerticalWordCandidate(i,j,blocks,input) :
         
          bottom,upper = getVerticalRangeIndex(i,j,blocks) 
@@ -325,20 +346,12 @@ blocks = [
 #           ['#','-','-','-','-'],
 #         ]
 
-#initializeBlocks(blocks,ROW,COL)
-#setVerticalValues(0,0,0,3,blocks,['*',"*","*","*"])
-#printArray(blocks)
-#print(getHorizontalRangeIndex(1,0,blocks))
-#print(getHorizontalRegexValid(0,0,blocks))
-#print(getVerticalRegexValid(0,1,blocks))
 
-
-#setHorizontalValues(0,0,0,3,blocks,['d','r','a','t'])
-#print(getNextIndex(0,0,blocks))
-#print(getHorizontalWordCandidate(0,3,blocks,input))
-#print(getVerticalWordCandidate(4,4,blocks,input))
 count = getCount(0,0,blocks)
 
 (i,j) = getNextIndex(0,0,blocks)
-crossWordSolver(i,j,blocks,input,count)
+#حل مسیله
+cehck = crossWordSolver(i,j,blocks,input,count)
 printArray(blocks)
+if cehck == False :
+    print("Not found")
